e976cb5409c051324b3d4fa456bb6517f51a6b9d
devcarol03 <devcarol03@gmail.com>
2023-09-20 22:48:24 +0100

    UPDATE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./public/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
